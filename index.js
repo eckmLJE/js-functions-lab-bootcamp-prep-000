63d2cc971ecc1863430ef97076128bd33af9bd43
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-07 16:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${string}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo(name,holiday) {
